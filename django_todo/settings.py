647334091ab2afeab6cc498c642964042cad1337
Dylan <dylankane3@gmail.com>
2023-06-19 17:55:13 +0000

    Try fix crash error

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEVELOPMENT = False
 
 
 ALLOWED_HOSTS = ['https://dmk-todo-app-d1bc52ffc75e.herokuapp.com']
