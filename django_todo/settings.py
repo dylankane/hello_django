eb411574fce3cd791833b10177a02b7f347c8283
Dylan <dylankane3@gmail.com>
2023-06-19 17:19:49 +0000

    Add updated allowed hosts for deployed site

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-dylankane-hellodjango-vu7vveiz3jo.ws-eu100.gitpod.io']
+ALLOWED_HOSTS = ['https://dmk-todo-app-d1bc52ffc75e.herokuapp.com/']
 
 
 # Application definition
